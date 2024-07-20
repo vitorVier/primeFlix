0564fba82519d08dcfde935338ce52983826d330
vitorVier <vierrvitor@gmail.com>
2024-07-20 17:55:51 -0300

    new-link

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -37,7 +37,7 @@ export default function Favoritos() {
                         <li key={item}>
                             <span>{item.title}</span>
                             <div>
-                                <Link to={`filme/${item.id}`}>Ver detalhes</Link>
+                                <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
                                 <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
